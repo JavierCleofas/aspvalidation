d4bc0a65c3b4c6619571081a3e9fb24c0d0a8da0
JavierC-ASEP <francisco.cleofas@auditoriapuebla.gob.mx>
2021-07-21 10:31:36 -0500

    se agrega api ejemplo

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+class ApiController extends Controller
+{
+
+    public function getRequest()
+    {
+        $client = new \GuzzleHttp\Client();
+    $response = $client->get('https://apisnet.col.gob.mx/wsSignGob/apiV1/Status');
+    $status = $response->getBody()->getContents();
+    $json = json_decode($status, true);
+    dd($json['RESTService']);
+
+        return view('welcome');
+    }    
+}
